456dddc3e9f8d9a81141665893bb3478e0e6e300
clinthof <73394309+clinthof@users.noreply.github.com>
2022-09-02 11:17:51 -0400

    Add basic template and stub out op functions

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,62 @@
 <template>
-  <div></div>
+  <div>
+    <div>
+      <textarea
+        type="text"
+        placeholder="Enter some text"
+        v-model="message"
+      ></textarea>
+      <br />
+      <button>Add</button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="(item, index) in list" :key="index">
+          <div v-if="!item.edit">
+            <p>{{ item.text }}</p>
+            <button>Edit</button>
+          </div>
+          <div v-else>
+            <textarea></textarea>
+            <br />
+            <button>Save</button>
+          </div>
+          <button>Delete</button>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface IlistItem {
+  text: string;
+  edit: boolean;
+}
+
+const message = ref("");
+const list = ref<IlistItem[]>([]);
+
+const addItem = (newItem: IlistItem) => {
+  return;
+};
+
+const deleteItem = (i: number) => {
+  return;
+};
+
+const editItem = (i: number) => {
+  return;
+};
+
+const saveItem = (i: number) => {
+  return;
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
